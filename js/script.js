9712a8150b5e52ff0fdf548d16408e397b5983ac
JNW365 <john_white_3@hotmail.com>
2025-03-16 12:07:46 -0500

    all elemnts added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // Add custom JavaScript here
+
+// Triggering Toast
+
+const toastTrigger = document.getElementById('place-order');
+const toast = document.getElementById('confirmation');
+
+if(toastTrigger) {
+    const confirmToast = bootstrap.Toast.getOrCreateInstance(toast);
+    toastTrigger.addEventListener('click', () => {
+        confirmToast.show();
+    })
+}
+
